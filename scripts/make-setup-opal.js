410be87d386d8091eb742cb55dddce7a1581a88f
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-06-01 14:26:21 +0200

    test

--- scripts/make-setup-opal.js
+++ scripts/make-setup-opal.js
@@ -1,10 +1,10 @@
 const shell = require('shelljs');
 const fs = require('fs');
 
-if (!fs.existsSync('./static/javascripts/setup-opal.js')) {
-    shell.mkdir('-p', './static/javascripts');
+if (!fs.existsSync('./javascripts/setup-opal.js')) {
+    shell.mkdir('-p', './javascripts');
     shell.cd('opal');
     const s = shell.cat('opal.min.js', 'config-opal.js', 'opal-parser.min.js', 'config-opal-parser.js');
     shell.cd('..');
-    fs.writeFileSync('./static/javascripts/setup-opal.js', s.toString());
+    fs.writeFileSync('./javascripts/setup-opal.js', s.toString());
 }
